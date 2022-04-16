68a47b717ee58e2787e6f48e60b27b18addc6c79
Aditya <arizka.ashar@gmail.com>
2022-04-16 19:40:01 +0700

    Create eht endpoints

--- Section 3 - Working with DBs/main.py
+++ Section 3 - Working with DBs/main.py
@@ -1,9 +1,11 @@
 import databases
 import sqlalchemy
+from fastapi import FastAPI, Request
+import uvicorn
 
 DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/store" 
 
-data = databases.Database(DATABASE_URL)
+database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
 
 
@@ -15,4 +17,37 @@ books = sqlalchemy.Table(
     sqlalchemy.Column("author", sqlalchemy.String),
 )
 
+engine = sqlalchemy.create_engine(DATABASE_URL)
+metadata.create_all(engine)
 
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+@app.get("/books/")
+async def get_all_books():
+    query = books.select()
+    return await database.fetch_all(query)
+
+@app.post("/books/")
+async def create_book(request: Request):
+    data = await request.json()
+    # query = books.insert().values(title = data["title"], author = data["author"] )
+    query = books.insert().values(**data)
+    last_record_id = await database.execute(query)
+    return {"id": last_record_id}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
